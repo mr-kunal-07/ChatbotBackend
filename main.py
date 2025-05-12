2a591304206afb7d4648088e456d95f0f87bf1cf
Kunal Jadhav <154059626+mr-kunal-07@users.noreply.github.com>
2025-05-12 17:23:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",  # for local dev
-        "https://chatbot-kuanl-jadhavs-projects.vercel.app",  # for production
+        "https://chatbot1-six-delta.vercel.app/",  # for production
     ],
     allow_credentials=True,
     allow_methods=["*"],
